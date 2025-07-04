1cc4956ba5e2878731be333c12728fe02261d414
Hamadeh <amr.grenoble@gmail.com>
2025-07-04 18:35:09 +0200

    :sparkles: Adding surface for poylgons

--- DistanceDisplayTool.py
+++ DistanceDisplayTool.py
@@ -1,10 +1,7 @@
-from PyQt5.QtCore import QObject, QEvent
+from PyQt5.QtCore import QObject, QEvent,QSizeF
 from PyQt5.QtGui import QTextDocument
-from qgis.core import QgsTextAnnotation
+from qgis.core import QgsTextAnnotation, QgsDistanceArea,QgsProject ,QgsGeometry, QgsWkbTypes
 from qgis.gui import QgsMapCanvasAnnotationItem
-from PyQt5.QtCore import QSizeF
-from qgis.core import QgsDistanceArea
-from qgis.core import QgsProject
 
 class DistanceDisplayController(QObject):
     def __init__(self, iface):
@@ -16,8 +13,7 @@ class DistanceDisplayController(QObject):
 
         self.distance_calculator = QgsDistanceArea()
         self.distance_calculator.setEllipsoid(QgsProject.instance().ellipsoid())
-        print(QgsProject.instance().ellipsoid())
-        self.distance_calculator.setSourceCrs(self.canvas.mapSettings().destinationCrs(),QgsProject.instance().transformContext())
+        self.distance_calculator.setSourceCrs(self.canvas.mapSettings().destinationCrs(), QgsProject.instance().transformContext())
 
     def eventFilter(self, obj, event):
         if obj == self.canvas.viewport() and event.type() == QEvent.MouseMove:
@@ -46,14 +42,24 @@ class DistanceDisplayController(QObject):
         mouse_point = self.canvas.getCoordinateTransform().toMapCoordinates(event.pos())
         distance = self.distance_calculator.measureLine(last_point, mouse_point)
 
-        self.show_distance(distance, mouse_point)
+        layer = self.iface.activeLayer()
+        surface = None
 
-    def show_distance(self, distance, position):
+        if layer and layer.geometryType() == QgsWkbTypes.PolygonGeometry and len(pts) >= 2:
+            polygon_points = pts + [mouse_point]
+            polygon_points.append(polygon_points[0])
+
+            polygon_geom = QgsGeometry.fromPolygonXY([polygon_points])
+            surface = self.distance_calculator.measureArea(polygon_geom)
+
+        self.show_measurements(distance, surface, mouse_point)
+
+    def show_measurements(self, distance, surface, position):
         self.clear_annotation()
 
         annotation = QgsTextAnnotation()
         annotation.setMapPosition(position)
-        annotation.setFrameSize(QSizeF(140, 40))
+        
         html_text = f"""
         <div style="
             background-color: rgba(30, 30, 30, 0.8);
@@ -64,14 +70,22 @@ class DistanceDisplayController(QObject):
             padding: 6px 12px;
             box-shadow: 3px 3px 8px rgba(0,0,0,0.5);
             ">
-            Distance: <span style="color:#4CAF50;">{distance:.2f} m</span>
-        </div>
-        """
+            Distance: <span style="color:#4CAF50;">{distance:.2f} m</span>"""
+
+        if surface is not None:
+            html_text += f"""<br/>
+            Surface: <span style="color:#2196F3;">{surface:.2f} m²</span>"""
+
+        html_text += "</div>"
 
         doc = QTextDocument()
         doc.setHtml(html_text)
         annotation.setDocument(doc)
 
+        if surface is not None:
+            annotation.setFrameSize(QSizeF(160, 60)) 
+        else:
+            annotation.setFrameSize(QSizeF(160, 35))  
         self.text_item = QgsMapCanvasAnnotationItem(annotation, self.canvas)
         self.text_item.setZValue(1000)
 
